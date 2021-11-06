05da47710329f51c84c473ac647f4229a54b48da
jhonpulido1990 <3825@holbertonschool.com>
2021-11-06 10:55:12 -0500

    add _putchar and main

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/*include library*/
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/*call a funtion _putchar*/
+int _putchar(char c);
+/*function that produces output according to a format*/
+int _printf(const char *format, ...);
+
+#endif /*HEADER_FILE*/
